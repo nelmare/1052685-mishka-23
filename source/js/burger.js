07626bded2c256ee37efb6948fb812c7144724ac
Marina Kuklina <nelmare@yandex.ru>
2021-08-14 16:37:08 +0300

    форматирует код

--- source/js/burger.js
+++ source/js/burger.js
@@ -2,15 +2,7 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
-// navToggle.classList.remove('main-nav__toggle--nojs');
-// navToggle.classList.add('main-nav__toggle--open');
-// navMainList.classList.add('main-nav--closed');
-//
-// navToggle.addEventListener('click', function () {
-//   navMainList.classList.toggle('main-nav--closed');
-//   navMainList.classList.toggle('main-nav--open');
-//   navToggle.classList.toggle('main-nav__toggle--open');
-//   navToggle.classList.toggle('main-nav__toggle--closed');
+
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
@@ -20,4 +12,3 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--open');
   }
 });
-
